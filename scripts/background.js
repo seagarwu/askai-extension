95c1285801a243c6a5330142d474dedebb1ea568
alanwu <seagarwu@gmail.com>
2025-07-21 11:41:38 +0800

    fix: Remove 'tabs' permission to comply with Chrome Store policy
    - Refactor background script to use  instead of .
    - Remove the "tabs" permission from manifest.json.
    
    This change addresses the "Purple Potassium" violation by adhering to the principle of least privilege, without affecting user-facing functionality.

--- scripts/background.js
+++ scripts/background.js
@@ -71,9 +71,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       }
     });
   } else if (request.type === "createPopup") {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {type: "showPopup", selectionText: request.selectionText, position: request.position});
-    });
+    chrome.tabs.sendMessage(sender.tab.id, {type: "showPopup", selectionText: request.selectionText, position: request.position});
   }
   return true;
 });
